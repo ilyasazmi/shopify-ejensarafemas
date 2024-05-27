33d3a31168b552905d4964154bf81eed6420a0f5
ilyasazmi <ilyasthemidas@yahoo.com>
2024-05-27 20:01:45 +0800

    saja2 update

--- app/components/ilyas/FooterMobile.jsx
+++ app/components/ilyas/FooterMobile.jsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 const FooterMobile = () => {
     return (
         <>
-            {/* <section className="bg-green-500 fixed bottom-32 right-2 z-20 p-1 rounded-lg border-2 border-green-50 md:hidden mb-2" > */}
+            {/* <section clasddName="bg-green-500 fixed bottom-32 right-2 z-20 p-1 rounded-lg border-2 border-green-50 md:hidden mb-2" > */}
             {/* <div className="grid grid-cols-1 gap-6 text-xs text-center p-2 "> */}
             <Link prefetch="viewport" className="text-white text-sm bg-green-500 p-2 h-10 w-20 mb-1 fixed bottom-32 right-2 z-20 rounded-lg border-2 border-green-50 md:hidden" to="https://api.whatsapp.com/send?phone=601169799840&text=%21ShareInfoSarafEmas">Whatsapp</Link>
             {/* </div> */}
